55a77ddd2f9c623375624ac107cc0c925d140169
GentaArya <mgentaarya@yahoo.com>
2023-09-16 01:10:57 +0700

    add loading icon

--- src/View/Splash/index.jsx
+++ src/View/Splash/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../../../src/Style/Splash.css";
+import FadeLoader from "react-spinners/FadeLoader";
 
 const SplashScreen = () => {
   return (
     <div className="splash-screen">
       <div className="splash-card">
         <h1 className="splash-text">
-          <span>Please wait </span>
+          <div className="splash-content">
+            <FadeLoader size={150} color="white" /> 
+          </div>
+          <span>Please wait</span>
         </h1>
       </div>
     </div>
