0152bfe68305229509c789c97bd1feb73a9d4abe
GentaArya <mgentaarya@yahoo.com>
2023-09-15 22:59:21 +0700

    add corousel

--- src/View/Home/Component/Product.jsx
+++ src/View/Home/Component/Product.jsx
@@ -1,43 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import products from "../../../Model/ProductData";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../../../Style/Content.css";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
-import "../../../Style/Content.css";
-
 function ProductLayout() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className=" container mx-auto mt-12 bg-gray-200 rounded-full p-8 border-2 pb-16 " id="productInfo">
-      <h2 className="text-3xl font-bold mb-4 text-center text-white bg-black w-60 rounded-full p-2 shines ">
+    <div
+      className="container mx-auto mt-12 bg-gray-200 rounded-full p-8 border-2 pb-16"
+      id="productInfo"
+    >
+      <h2 className="text-3xl font-bold mb-4 text-center text-white bg-black w-60 rounded-full p-2 shines">
         Our Products
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="bg-white p-4 rounded-lg shadow-md relative drop-shadow-2xl  "
-          >
-            <img
-              src={product.imageUrl}
-              alt={product.title}
-              className="w-full h-40 rounded-md mb-4 "
-            />
-            <h3 className="text-xl font-semibold mb-2">{product.title}</h3>
-            <p className="text-gray-600 mb-4">{product.description}</p>
-            <p className="text-lg font-semibold">{product.price}</p>
-            <ScrollLink
-              to="contactSection" 
-              spy={true}
-              smooth={true}
-              offset={-70} 
-              duration={2500}
-              className="bg-gray-700 text-white px-4 py-2 mt-4 rounded-md hover:bg-gray-800 absolute bottom-4 right-4 animate-none hover:scale-110 transition-all delay-75"
-              style={{ cursor: "pointer" }}
+      {isMobile ? (
+        <Carousel
+          showThumbs={false}
+          infiniteLoop={true}
+          showStatus={false}
+          swipeable={true}
+          emulateTouch={true}
+          className="mobile-carousel"
+        >
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white p-4 rounded-lg shadow-md relative drop-shadow-2xl"
             >
-              Order Now
-            </ScrollLink>
-          </div>
-        ))}
-      </div>
+              <img
+                src={product.imageUrl}
+                alt={product.title}
+                className="w-full h-40 rounded-md mb-4"
+              />
+              <h3 className="text-xl font-semibold mb-2">{product.title}</h3>
+              <p className="text-gray-600 mb-4">{product.description}</p>
+              <p className="text-lg font-semibold">{product.price}</p>
+              <ScrollLink
+                to="contactSection"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={2500}
+                className="bg-gray-700 text-white px-4 py-2 mt-2 rounded-md hover:bg-gray-800 text-center block mb-8"
+                style={{ cursor: "pointer" }}
+              >
+                Order Now
+              </ScrollLink>
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <div className={`grid sm:grid-cols-2 md:grid-cols-3 gap-6`}>
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className={`bg-white p-4 rounded-lg shadow-md relative drop-shadow-2xl`}
+            >
+              <img
+                src={product.imageUrl}
+                alt={product.title}
+                className="w-full h-40 rounded-md mb-4"
+              />
+              <h3 className="text-xl font-semibold mb-2">{product.title}</h3>
+              <p className="text-gray-600 mb-4">{product.description}</p>
+              <p className="text-lg font-semibold">{product.price}</p>
+              <ScrollLink
+                to="contactSection"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={2500}
+                className="bg-gray-700 text-white px-4 py-2 mt-4 rounded-md hover:bg-gray-800 absolute bottom-4 right-4 animate-none hover:scale-110 transition-all delay-75"
+                style={{ cursor: "pointer" }}
+              >
+                Order Now
+              </ScrollLink>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/View/Jobs/Index.jsx
+++ src/View/Jobs/Index.jsx
@@ -7,6 +7,9 @@ import Footer from "../Home/Component/Footer";
 import HeaderJob from "./Component/Header";
 import { FaShare } from "react-icons/fa";
 import "../../../src/Style/JobList.css";
+import "../../../src/Style/Content.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function getRandomImageURL(width, height) {
   const imageId = Math.floor(Math.random() * 1084) + 1;
@@ -89,13 +92,7 @@ const JobList = () => {
     };
   }, []);
 
-  const scrollToTop = () => {
-    scroll.scrollToTop();
-  };
-
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
+ 
 
   const filteredJobs = jobs.filter((job) =>
     job.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -125,70 +122,68 @@ const JobList = () => {
         <div className="flex items-center justify-between p-6 border-b border-gray-300 ">
           <h2 className="text-3xl font-bold job-list-heading">Job List</h2>
         </div>
-        <div className="custom-grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-6">
-          {loading ? (
-            <div className="loader-container ">
-              <div className="custom-loader">
-                <div className="loader" />
-              </div>
-            </div>
-          ) : error ? (
-            <p className="text-red-600">{error}</p>
-          ) : filteredJobs.length === 0 ? (
-            <p className="text-red-600 col-span-3 text-center no-matching-jobs-message">
-              No jobs match your search.
-            </p>
-          ) : (
-            filteredJobs.map((job) => (
-              <div key={job.id} className="custom-card">
-                <div className="card-inner ">
-                  <div className="card-face">
-                    <img
-                      src={getRandomImageURL(300, 300)}
-                      alt="Job Image"
-                      className="w-24 h-24 rounded-full mb-4 mx-auto"
-                    />
-                    <h3 className="text-xl font-semibold text-center mb-2">
-                      {job.title}
-                    </h3>
-                    <p className="text-gray-500 text-center">{job.company}</p>
-                    <p className="text-gray-400 text-center text-xs mt-2">
-                      Posted: {job.postDate}
-                    </p>
-                  </div>
-                  <div className="card-face card-back ">
-                    <a
-                      href={job.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="block w-full text-center bg-gray-700 text-white py-2 rounded hover:bg-gray-800 "
-                    >
-                      Apply
-                    </a>
-                  </div>
-                  <div className="relative">
-                    <FaShare
-                      className="absolute  text-gray-600 cursor-pointer hover:text-gray-800 "
-                      title="Share"
-                      onClick={() => handleShare(job.url)}
-                    />
+      </div>
+      {loading ? (
+        <div className="loader-container ">
+          <div className="custom-loader">
+            <div className="loader" />
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="p-2">
+            <Carousel
+              showThumbs={false}
+              showStatus={false}
+              infiniteLoop={true}
+              emulateTouch={true}
+              className="mobile-carousel"
+            >
+              {filteredJobs.map((job) => (
+                <div key={job.id} className="custom-card">
+                  <div className="card-inner">
+                    <div className="card-face">
+                      <img
+                        src={getRandomImageURL(300, 300)}
+                        alt="Job Image"
+                        className="w-full h-100 rounded-lg mb-4 mx-auto"
+                      />
+                      <h3 className="text-xl font-semibold text-center mb-2">
+                        {job.title}
+                      </h3>
+                      <p className="text-gray-500 text-center">{job.company}</p>
+                      <p className="text-gray-400 text-center text-xs mt-2 mb-12">
+                        Posted: {job.postDate}
+                      </p>
+                    </div>
+                    <div className="card-face card-back">
+                      <a
+                        href={job.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block w-full text-center bg-gray-700 text-white py-2 rounded hover:bg-gray-800"
+                      >
+                        Apply
+                      </a>
+                    </div>
+                    <div className="relative">
+                      <FaShare
+                        className="absolute  text-gray-600 cursor-pointer hover:text-gray-800"
+                        title="Share"
+                        onClick={() => handleShare(job.url)}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
-          )}
-        </div>
-      </div>
-
-      <Footer />
-      {showScrollButton && (
-        <button
-          className="fixed bottom-12 right-10 w-14 h-14 bg-gray-700 text-white flex items-center justify-center  rounded-full cursor-pointer hover:bg-gray-500"
-          onClick={scrollToTop}
-        >
-          <FontAwesomeIcon icon={faArrowUp} />
-        </button>
+              ))}
+            </Carousel>
+          </div>
+        </>
       )}
+
+      <div className="mt-12">
+        <Footer />
+      </div>
     </div>
   );
 };
